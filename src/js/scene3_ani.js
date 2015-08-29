cd576ee26907494c22a07da842520934c6a5e1b1
disoul <starky_ds@sina.cn>
2015-08-30 01:11:30 +0800

    add animate

---
+++ src/js/scene3_ani.js
@@ -0,0 +1,14 @@
+var aniFlag = 0;
+
+$(document).ready(function(){
+	$("#main-scene").on("tap",function(){
+		if (aniFlag != 0) return; 
+		aniFlag++;
+		var listHeight = $(".left-list").height();
+		$(".left-list").css("top","-"+listHeight.toString()+"px");
+		$(".left-list").animate({top: "0"},"slow");
+
+		$(".right-list").css("bottom","-"+listHeight.toString()+"px");
+		$(".right-list").animate({bottom: "0"},"slow");
+	});
+});
