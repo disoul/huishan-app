87576ddfa98baba67834da4a80ef4ec9bfcfdf6e
disoul <starky_ds@sina.cn>
2015-08-30 15:38:44 +0800

    add js

--- src/js/scene3_control.js
+++ src/js/scene3_control.js
@@ -1,13 +1,36 @@
 var domCreate = function(step){
 	if (step == 1){
-		$(".left-iron").remove();
-		var left_list_control = '<div id="control" class="left-list"></div>';
+		//Creat Left-List
+		$("#left-control").fadeOut();
+		var left_list_control = '<div id="left-control" class="left-list"></div>';
 		$("#main-scene").append(left_list_control);
-		$("#control").css("height",$("#img").height().toString()+"px");
+		$("#left-control").css("height",$("#left-img").height().toString()+"px");
 		var face1_select = '<div class="left-iron" id="face1_select"><img src="./src/image/main-scene/face_select_1.png" alt=""></div>';
 		var face2_select = '<div class="left-iron" id="face2_select"><img src="./src/image/main-scene/face_select_2.png" alt=""></div>';
-		$("#control").append(face1_select);
-		$("#control").append(face2_select);
+		$("#left-control").append(face1_select);
+		$("#left-control").append(face2_select);
 		$(".left-iron").css("height",$(".left-iron").css("width"));
+
+
+		//Creat Top-List
+		$("#top-mask").css("height",$("#top-img").height().toString()+"px");
+
+
+		//Add Events
+		$("#face1_select").on("tap",function(){
+			$("#face").fadeOut();
+			$("#face").remove();
+			var face_decoration = '<div style="display:none;" id="face" class="model"><img src="./src/image/main-scene/face1.png" alt=""></div>';
+			$(".canvas").append(face_decoration);
+			$("#face").fadeIn();
+		});
+		$("#face2_select").on("tap",function(){
+			$("#face").fadeOut();
+			$("#face").remove();
+			var face_decoration = '<div style="display:none;" id="face" class="model"><img src="./src/image/main-scene/face2.png" alt=""></div>';
+			$(".canvas").append(face_decoration);
+			$("#face").fadeIn();
+		});
+
 	}
 }
---
+++ src/js/scene3_pre.js
@@ -0,0 +1,17 @@
+var aniFlag = 0;
+
+$(document).on("pageinit","#main-scene",function(){
+	$("#main-scene").on("tap",function(){
+		if (aniFlag != 0) return; 
+		aniFlag++;
+		domCreate(1);
+		var listHeight = $(".left-list").height();
+		$(".left-list").css("top","-"+listHeight.toString()+"px");
+		$(".left-list").animate({top: "0"},"slow");
+
+		$(".right-list").css("bottom","-"+listHeight.toString()+"px");
+		$(".right-list").animate({bottom: "0"},"slow");
+
+	});
+
+});
